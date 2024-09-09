02f2076f8beeb687a3febac2bbd3d0112784c76e
Nabil <nabilshaikh26@gmail.com>
2024-09-09 20:55:14 +0530

    Configured the cucumber library

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require ("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    setupNodeEvents,
+    specPattern: "cypress/e2e/**/*.{feature,cy.js}"
   },
 });
